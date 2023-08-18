7df5708a4c357791af787acef00ca5c36a6b7792
TuanDinh140194 <tnd@pdx.edu>
2023-08-18 11:31:59 -0700

    update movie watch episode

--- pages/watch/[movieId].tsx
+++ pages/watch/[movieId].tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import useMovie from "@/hooks/useMovie";
 import { useRouter } from "next/router";
 import { AiOutlineArrowLeft } from "react-icons/ai";
+import { TbBoxMultiple } from "react-icons/tb";
 
 const Watch = () => {
   const router = useRouter();
@@ -10,10 +11,28 @@ const Watch = () => {
   const { data } = useMovie(movieId as string);
 
   const [selectedEpisode, setSelectedEpisode] = useState(0);
+  const [isEpisodeMenuOpen, setIsEpisodeMenuOpen] = useState(false); // State to control the episode menu
+  const episodeButtonRef = useRef(null);
 
-  const nextEpisode = () => {
-    setSelectedEpisode((prevEpisode) => prevEpisode + 1);
+  const handleEpisodeSelect = (episodeIndex: number) => {
+    setSelectedEpisode(episodeIndex);
+    // setIsEpisodeMenuOpen(false); // Close the episode menu
   };
+
+  // Close the episode menu when clicking outside
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (isEpisodeMenuOpen && episodeButtonRef.current) {
+        setIsEpisodeMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [isEpisodeMenuOpen]);
 
   return (
     <div className="h-screen w-screen bg-black">
@@ -35,7 +54,7 @@ const Watch = () => {
           className="text-white cursor-pointer"
           size={40}
         />
-        <p className="text-white text-1xl md:text-3xl font-bold">
+        <p className="text-white text-1xl md:text-3xl font-bold hidden sm:block">
           <span className="font-light mr-2">Watching:</span>
           {data?.title}
         </p>
@@ -47,17 +66,38 @@ const Watch = () => {
           className="h-full w-full"
           src={data?.movieUrl[selectedEpisode]}
         ></video>
-        <div className="absolute top-4 right-40 bg-white p-2 bg-opacity-50 rounded mx-4 z-20">
-          Episode {selectedEpisode + 1} / {data?.movieUrl.length}
-        </div>
       </div>
-      {data && selectedEpisode < data.movieUrl.length - 1 && (
-        <button
-          onClick={nextEpisode}
-          className="absolute top-4 right-4 bg-white bg-opacity-50 hover:bg-white px-4 py-2 rounded z-20"
-        >
-          Next Episode
-        </button>
+      {data && data.movieUrl.length > 1 && (
+        <div>
+          <div className="absolute top-4 right-20 bg-gray-600  text-white p-2 bg-opacity-50 rounded mx-4 z-20 hidden lg:block">
+            Episode {selectedEpisode + 1} / {data?.movieUrl.length}
+          </div>
+          <button
+            onMouseEnter={() => setIsEpisodeMenuOpen(true)}
+           
+            ref={episodeButtonRef}
+            className="absolute top-4 right-10 text-white hover:scale-125 px-4 py-2 rounded z-20 md:px-0"
+          >
+            <TbBoxMultiple size={25} />
+            <span>
+              {isEpisodeMenuOpen && (
+                <div className="absolute top-8 right-4 bg-gray-600 bg-opacity-40 mt-4 w-40 rounded z-20">
+                  {data?.movieUrl.map((url: string, index: number) => (
+                    <div
+                      key={index}
+                      onClick={() => handleEpisodeSelect(index)}
+                      className={`cursor-pointer px-6 py-2 ${
+                        selectedEpisode === index ? "font-bold bg-black" : ""
+                      } hover:bg-gray-500`}
+                    >
+                      Episode {index + 1}
+                    </div>
+                  ))}
+                </div>
+              )}
+            </span>
+          </button>
+        </div>
       )}
     </div>
   );
